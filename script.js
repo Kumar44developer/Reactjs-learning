fca4f8e8c25381a0ace10384df220416bb576620
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-13 21:26:11 +0530

    Writing the script by Using Math.random() Function

--- script.js
+++ script.js
@@ -9,7 +9,13 @@ ReactDOM.render(
   <div>
     <h1>Hello {fName}!</h1>
     <p>Your lucky number is {3 + 4}</p>
-   
+      <p>Your Random Number is {Math.floor(Math.random() * 10)}</p>
+    {/* genrating the random number  */}
+    {/* see here we have to use the code like this itself */}
+    {/* you can add any javascript expression inbetween this curly braces */}
+    {/* see we are going learning the expressin here */}
+    {/* see 9here we are learning the addition of the two elements */}
+    <p>Hello {fName} {lName}!</p>
     
   </div>,
   document.getElementById("root")
