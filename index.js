4d07ea209eb799bca77dd0ab30f8bcd3ca3bbd73
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-14 09:05:16 +0530

    Variable Declaration and Initialization

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// see here if i write like this
+//this wil throw the error to me
+// importReactfrom"react";
+// importReactDOMfrom"rect-dom";
+const name = "Angela";
+const currentDate = new Date();
+
+
+
+
+
+
+
+
+
 
