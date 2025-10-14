400714155a7754a87b176e9b7084a27444fc1ac6
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-10-14 09:08:47 +0530

    Writing the Render Function in JS file

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ import ReactDOM from "react-dom";
 const name = "Angela";
 const currentDate = new Date();
 const year = currentDate.getMonth();
+ReactDOM.render(
+  <div>
+    <p>Created By {name}</p>
+    <p>Copyright {year}</p>
 
 
 
