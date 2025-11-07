823566f00d0207a8a56511c869d16898e1078be9
Kumar Swamy R Y <kumarswamyry82@gmail.com>
2025-11-07 10:11:20 +0530

    Rendered footer with author name and current year using ReactDOM

--- index1.js
+++ index1.js
@@ -4,3 +4,8 @@ import ReactDOM from "react-dom";
 const name = "Angela";
 const currentDate = new Date();
 const year = currentDate.getMonth();
+ReactDOM.render(
+  <div>
+    <p>Created By {name}</p>
+    <p>Copyright {year}</p>
+  </div>,
